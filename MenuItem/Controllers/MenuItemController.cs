07170de296c489435adbe129673f629eb5aa35fc
sreelakshmipradeep20 <38486733+sreelakshmipradeep20@users.noreply.github.com>
2022-05-19 15:48:55 +0530

    Update MenuItemController.cs

--- MenuItem/Controllers/MenuItemController.cs
+++ MenuItem/Controllers/MenuItemController.cs
@@ -16,8 +16,8 @@ namespace MenuItemListing.Controllers
         public List<MenuItem> GetItem()
         {
             List<MenuItem> MenuList = new List<MenuItem>()
-            {   new MenuItem() {Id=1, Name="Phone", Active=true, DateOfLaunch=new DateTime(2017,01,01), FreeDelivery=false,Price=500},
-                new MenuItem() {Id=2, Name="LapTop", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=200}
+            {   new MenuItem() {Id=1, Name="Pizza", Active=true, DateOfLaunch=new DateTime(2017,01,01), FreeDelivery=false,Price=500},
+                new MenuItem() {Id=2, Name="Shakes", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=200}
 
             };
             return MenuList;
@@ -27,8 +27,8 @@ namespace MenuItemListing.Controllers
         public MenuItem GetItemById(int id)
         {
             List<MenuItem> MenuList = new List<MenuItem>()
-            {   new MenuItem() {Id=1, Name="Phone", Active=true, DateOfLaunch=new DateTime(2017,01,01), FreeDelivery=false,Price=500},
-                new MenuItem() {Id=2, Name="Laptop", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=200}
+            {   new MenuItem() {Id=1, Name="Pizza", Active=true, DateOfLaunch=new DateTime(2017,01,01), FreeDelivery=false,Price=500},
+                new MenuItem() {Id=2, Name="Shakes", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=200}
 
             };
             MenuItem obj = MenuList.SingleOrDefault(item => item.Id == id);
